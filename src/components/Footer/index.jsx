3f587429c05927ae62dc181c692b5ac8e79536cf
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-11-29 15:34:03 +0100

    Project files

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-dark-navy-blue w-full py-10 flex justify-center align-middle text-white text-1xl">
+      <p>Holidaze - 2025 Ephraim Djeket</p>
+    </div>
+  )
+}
+
+export default Footer;
