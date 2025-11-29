35c1ddacb3064493fe53ba3236854a2d0f87c78e
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-11-29 15:32:46 +0100

    App.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
