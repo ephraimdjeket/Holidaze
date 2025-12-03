1257bdfa67c4e87c5ef7cbc6206e1c3036582e02
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-12-03 16:14:09 +0100

    Deleted files

--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <div className="bg-dark-navy-blue w-full py-10 flex justify-center align-middle text-white text-1xl">
-      <p>Holidaze - 2025 Ephraim Djeket</p>
-    </div>
-  )
-}
-
-export default Footer;
