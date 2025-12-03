16a256da6af0224a656cc63abe51e0caf01eaa44
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-12-03 16:13:19 +0100

    Imported more components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Home from "./components/Home";
+import Navbar from "./components/pages/Navbar";
+import Footer from "./components/pages/Footer";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import Home from "./components/pages/Home";
+import Accomodation from "./components/pages/Accomodation";
+import Contact from "./components/pages/Contact";
+import Profile from "./components/pages/Profile";
+import VenueDashboard from "./components/pages/VenueDashboard";
+import ViewDetail from "./components/pages/ViewDetail";
 
 const App = () => {
 
   return (
     <>
       <Navbar />
-      <Home />
+      <ViewDetail />
       <Footer />
     </>
   )
