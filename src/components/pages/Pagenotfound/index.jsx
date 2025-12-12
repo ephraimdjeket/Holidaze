9daf28fa21d1839c7c6e94150e848f05b61f2e18
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-12-12 15:15:27 +0100

    Default page not found

---
+++ src/components/pages/Pagenotfound/index.jsx
@@ -0,0 +1,11 @@
+import pagenotfound from "../../../assets/404-icon.png";
+
+const Pagenotfound = () => {
+  return (
+    <div className="flex items-center justify-center min-h-[88.6vh]">
+      <img className="max-w-full w-xl mt-20" src={pagenotfound} alt="404 illustration image" />
+    </div>
+  )
+}
+
+export default Pagenotfound;
