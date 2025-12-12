05f7a4c84d05bba94bc036840bc748cbfad21947
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-12-12 15:17:05 +0100

    Added loader state

---
+++ src/components/ui/Loader/index.jsx
@@ -0,0 +1,12 @@
+import { OrbitProgress } from "react-loading-indicators";
+
+const Loader = () => {
+  return (
+    <OrbitProgress
+      color={["#0935b7"]}
+      size="medium"
+    />
+  );
+};
+
+export default Loader;
