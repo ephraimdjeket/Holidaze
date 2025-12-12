ba5f014561e3c0e945cef197ce56c215bdb1fbdc
Ephraim Djeket <ephdje50146@stud.noroff.no>
2025-12-12 15:14:37 +0100

    React router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Navbar from "./components/pages/Navbar";
+import { Routes, Route, Link } from "react-router-dom";
+import Header from "./components/pages/Header";
 import Footer from "./components/pages/Footer";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
@@ -8,15 +9,25 @@ import Contact from "./components/pages/Contact";
 import Profile from "./components/pages/Profile";
 import VenueDashboard from "./components/pages/VenueDashboard";
 import ViewDetail from "./components/pages/ViewDetail";
+import Pagenotfound from "./components/pages/PageNotFound";
+import Layout from "./components/Layout";
 
 const App = () => {
 
   return (
-    <>
-      <Navbar />
-      <ViewDetail />
-      <Footer />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="home" element={<Home />} />
+        <Route path="accomodations" element={<Accomodation />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="venue-dashboard" element={<VenueDashboard />} />
+        <Route path="View-detail" element={<ViewDetail />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ src/components/pages/Header/index.jsx
@@ -0,0 +1,47 @@
+import { Link } from "react-router-dom";
+import logo from "../../../assets/Holidaze-logo.png"
+import { useState } from "react";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <header className="w-full py-5 border-b fixed z-50 bg-white px-10">
+      <nav className="flex justify-between  max-w-7xl mx-auto">
+        <div className="flex gap-2 items-center">
+          <img src={logo} alt="Holidaze logo" className=" w-8" />
+          <h1 className="font-poppins text-primary-blue text-h1 hidden md:block"><a href="#">Holidaze</a></h1>
+        </div>
+
+        <svg onClick={() => setOpen(prev => !prev)} className="w-6 cursor-pointer md:hidden" fill="#000000" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1920 1411.412v225.882H0v-225.882h1920Zm0-564.706v225.882H0V846.706h1920ZM1920 282v225.882H0V282h1920Z" fill-rule="evenodd"></path> </g></svg>
+
+        <ul className="hidden md:flex gap-3 font-rubik items-center text-p">
+          <li className="hover:border-b"><Link to="/home">Home</Link></li>
+          <li className="hover:border-b"><Link to="/accomodations">Accomodations</Link></li>
+          <li className="hover:border-b"><Link to="/contact">Contact</Link></li>
+          <li className="hover:border-b"><Link to="/profile">Profile</Link></li>
+          <li className="hover:border-b"><Link to="/login">Logout</Link></li>
+        </ul>
+
+        <ul className={`${open ? "block" : "hidden"} md:hidden bg-white  fixed inset-0 z-5 font-poppins`}>
+          <div className="flex justify-between px-10">
+            <div className="flex flex-col gap-10 text-2xl mt-30">
+              <li><Link to="/home">Home</Link></li>
+              <li><Link to="/accomodations">Accomodations</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+              <li><Link to="/profile">Profile</Link></li>
+              <li><Link to="/login">Logout</Link></li>
+            </div>
+            <svg onClick={() => setOpen(prev => !prev)} className="block md:hidden w-10 mb-auto mt-3 -mr-3 cursor-pointer" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18.8,16l5.5-5.5c0.8-0.8,0.8-2,0-2.8l0,0C24,7.3,23.5,7,23,7c-0.5,0-1,0.2-1.4,0.6L16,13.2l-5.5-5.5 c-0.8-0.8-2.1-0.8-2.8,0C7.3,8,7,8.5,7,9.1s0.2,1,0.6,1.4l5.5,5.5l-5.5,5.5C7.3,21.9,7,22.4,7,23c0,0.5,0.2,1,0.6,1.4 C8,24.8,8.5,25,9,25c0.5,0,1-0.2,1.4-0.6l5.5-5.5l5.5,5.5c0.8,0.8,2.1,0.8,2.8,0c0.8-0.8,0.8-2.1,0-2.8L18.8,16z"></path> </g></svg>
+          </div>
+          <img
+            className="fixed bottom-4 left-1/1 transform -translate-x-1/2"
+            src={logo}
+            alt="Holidaze logo"
+          />
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
