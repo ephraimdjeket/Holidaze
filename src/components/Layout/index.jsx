e62c27f5c897aa8de4f92851558da20862fe19c8
Ephraim Djeket <ephdje50146@stud.noroff.no>
2026-01-12 17:07:36 +0100

    Passing props

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom";
 import Header from "../pages/Header";
 import Footer from "../pages/Footer";
 
-const Layout = () => {
+const Layout = ({ viewImage, setViewImage }) => {
   return (
     <>
-      <Header />
+      <Header viewImage={viewImage} setViewImage={setViewImage} />
       <Outlet />
       <Footer />
     </>
