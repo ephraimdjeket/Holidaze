703f89c533b4d6552800154e1d5d7b067f518363
Ephraim Djeket <ephdje50146@stud.noroff.no>
2026-01-12 17:06:44 +0100

    API endpoints

--- src/api/config.mjs
+++ src/api/config.mjs
@@ -1,3 +1,7 @@
 export const API_BASE = "https://v2.api.noroff.dev/";
 export const AUTH_LOGIN = "auth/login";
 export const AUTH_REGISTER = "auth/register";
+export const HOLIDAZE_VENUES = "holidaze/venues";
+export const CREATE_BOOKING = "holidaze/bookings";
+export const UPDATE_PROFILE = "holidaze/profiles";
+export const PROFILE_BOOKINGS = "/bookings"
