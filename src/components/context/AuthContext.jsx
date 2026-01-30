c765ab55b00aa864a618b397ccc7ca92de2258af
Ephraim Djeket <ephdje50146@stud.noroff.no>
2026-01-30 22:44:35 +0100

    Refractored AuthContext

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -8,25 +8,17 @@ export function UseAuth() {
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const value = {
-    user,
-    setUser,
-    isLoggedIn,
-    setIsLoggedIn
-  };
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
-  }, [])
+  }, []);
 
   return (
-    <AuthContext.Provider value={value}>
+    <AuthContext.Provider value={{ user, setUser }}>
       {children}
     </AuthContext.Provider>
-  )
-} 
+  );
+}
